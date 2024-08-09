e05d6a7de788aea0f52b955ee16bc3a5a2b024ca
wsarori <wsarori@gmail.com>
2024-08-09 02:47:31 -0400

    Implemented API endpoints

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -10,13 +10,18 @@ dotenv.config();
 const logger = pino();  // Create a Pino logger instance
 
 //Create JWT secret
+dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res) => {
     try {
       //Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
       const db = await connectToDatabase();
+
+      //Access the `users` collection
       const collection = db.collection("users");
+
+      //Check for existing email in DB
       const existingEmail = await collection.findOne({ email: req.body.email });
 
         if (existingEmail) {
@@ -27,6 +32,8 @@ router.post('/register', async (req, res) => {
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
         const email=req.body.email;
+
+        //Save user details
         const newUser = await collection.insertOne({
             email: req.body.email,
             firstName: req.body.firstName,
@@ -41,6 +48,7 @@ router.post('/register', async (req, res) => {
             },
         };
 
+        //Create JWT
         const authtoken = jwt.sign(payload, JWT_SECRET);
         logger.info('User registered successfully');
         res.json({ authtoken,email });
@@ -48,6 +56,49 @@ router.post('/register', async (req, res) => {
         logger.error(e);
         return res.status(500).send('Internal server error');
     }
+});
+
+    //Login Endpoint
+router.post('/login', async (req, res) => {
+    console.log("\n\n Inside login")
+
+    try {
+        // connect to `secondChance` in MongoDB through `connectToDatabase`
+        const db = await connectToDatabase();
+        //Access MongoDB `users` collection
+        const collection = db.collection("users");
+        //Check for user credentials in database
+        const theUser = await collection.findOne({ email: req.body.email });
+        //Check if the password matches
+        if (theUser) {
+            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            //send appropriate message if mismatch
+            if(!result) {
+                logger.error('Passwords do not match');
+                return res.status(404).json({ error: 'Wrong pasword' });
+            }
+            //Fetch user details
+            let payload = {
+                user: {
+                    id: theUser._id.toString(),
+                },
+            };
+
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+            //Create JWT authentication if passwords match
+            const authtoken = jwt.sign(payload, JWT_SECRET);
+            logger.info('User logged in successfully');
+            return res.status(200).json({ authtoken, userName, userEmail });
+        //Send appropriate message if user not found
+        } else {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).json({ error: 'Internal server error', details: e.message });
+      }
 });
 
 module.exports = router;
